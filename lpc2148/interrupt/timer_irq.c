c28a373ab8a8a4309b14802dda89f90e2613974e
wangdetash <wangdetash@gmail.com>
2017-08-26 19:04:02 +0530

    program to set port0 on timer with vectored interrupt

--- lpc2148/interrupt/timer_irq.c
+++ lpc2148/interrupt/timer_irq.c
@@ -1,4 +1,4 @@
-//program to set port0 on timer interrupt
+//program to set port0 on timer with vectored interrupt
 #include <LPC214X.H>
 
 void timer()__irq
@@ -16,7 +16,7 @@ T0PR=12000000;
 T0MR0=1;
 T0TCR=0X01;
 T0MCR=(1<<1)|(1<<0);
-VICIntEnable=1<<4;
+VICIntEnable=(1<<4);
 VICVectCntl0=(1<<5)|4;
 VICVectAddr0=(unsigned)timer;
 while(1);
