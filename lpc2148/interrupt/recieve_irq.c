2da02792de0c48693ea4de8cae47969f5e3de81e
wangdetash <wangdetash@gmail.com>
2017-08-26 13:06:56 +0530

    program to recieve charecter with interrupt

---
+++ lpc2148/interrupt/recieve_irq.c
@@ -0,0 +1,21 @@
+//program to recieve charecter with interrupt
+#include <LPC214X.H>
+void recieve()__irq
+{
+U0THR=U0RBR;
+while(!(U0LSR&0X20));
+VICVectAddr=0;
+}
+void main()
+{
+PINSEL0=0X00000005;
+IODIR0=0X00000001;
+U0LCR=0X83;
+U0DLL=97;
+U0LCR=0X03;
+U0IER=0X01;
+VICIntEnable=1<<6;
+VICVectCntl0=(1<<5)|6;
+VICVectAddr0=(unsigned)recieve;
+while(1);
+}
