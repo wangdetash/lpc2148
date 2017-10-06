705a520e0556c4e34158054a3860a1ce62078d9b
wangdetash <wangdetash@gmail.com>
2017-10-06 18:55:10 +0530

    program to use esp8266 to write a value to thingspeak continously

--- lpc2148/esp/esp.c
+++ lpc2148/esp/esp.c
@@ -6,8 +6,10 @@
 void transmit(char *);
 void recieve(char *);
 
-char apikey[]="0x9T........";
-char host_key[]="smart_Highway";
+char SSID[]="quest";
+char pass_word[]="quest123";
+char apikey[]="5DET0BBI9UP99QDC";
+char host_key[]="SMART HIGHWAY";
 char password[]="JaNisa";
 
 char data1[100],data2[100];
@@ -22,22 +24,22 @@ void main()
 	U0DLL=97;
 	U0LCR=0X03;
 
-	sprintf(data1,"AT+CWMODE=3");
+	sprintf(data1,"AT+CWMODE=3\n");
 	transmit(data1);
 	recieve(data1);
 
-	sprintf(data1,"AT+CWJAP=\"SSID\", \"password\"");
+	sprintf(data1,"AT+CWJAP=\"%s\", \"%s\"\n",SSID,pass_word);
 	transmit(data1);
 	recieve(data1);
 	while(1)
 	{	
-		sprintf(data1,"AT+CIPSTART=\"TCP\",\"192.168.3.116\", 8080");
+		sprintf(data1,"AT+CIPSTART=\"TCP\",\"192.168.3.116\", 8080\n");
 		transmit(data1);
 		recieve(data1);
 
-		sprintf(data2,"Get /update?api_key=%s&field1=%d",apikey,gas_level);
+		sprintf(data2,"Get /update?api_key=%s&field1=%d\n",apikey,gas_level);
 		string_length=strlen(data2);
-		sprintf(data1,"AT+CIPSEND=%d",string_length);
+		sprintf(data1,"AT+CIPSEND=%d\n",string_length);
 		transmit(data1);
 		transmit(data2);
 
